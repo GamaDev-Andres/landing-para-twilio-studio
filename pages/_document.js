82e57abb24c51c07e7d0dda9fec4af7e3a441aa3
gama_pipe@hotmail.com <gamadev@LAPTOP-5IO84K7D.localdomain>
2022-07-24 18:31:04 -0500

    setup initial

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>Twilio Studio</title>
+        <meta
+          name="description"
+          content="Ahora cualquier desarrollador puede crear, editar y administrar visualmente flujos de trabajo de comunicación en Twilio Studio, su generador de aplicaciones de código bajo/sin código. Arrastre y suelte widgets para crear aplicaciones de voz, mensajería y comunicaciones, y guarde el trabajo de codificación para tareas de orden superior."
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
